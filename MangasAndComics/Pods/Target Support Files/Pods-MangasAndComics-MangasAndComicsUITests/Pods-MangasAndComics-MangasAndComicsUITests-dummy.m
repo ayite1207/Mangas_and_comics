f572d0f4effe0ca264a64a7420f706e1bb198a18
afollySqli <afolly@sqli.com>
2023-01-11 23:23:19 +0100

    add firebase

---
+++ MangasAndComics/Pods/Target Support Files/Pods-MangasAndComics-MangasAndComicsUITests/Pods-MangasAndComics-MangasAndComicsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MangasAndComics_MangasAndComicsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MangasAndComics_MangasAndComicsUITests
+@end
