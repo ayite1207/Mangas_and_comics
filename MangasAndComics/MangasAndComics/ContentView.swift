8b5d71ba95f71b9f9599708c5db70040c53b7ef5
afollySqli <afolly@sqli.com>
2023-01-11 23:23:19 +0100

    add firebase

--- MangasAndComics/MangasAndComics/ContentView.swift
+++ MangasAndComics/MangasAndComics/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import FirebaseAnalytics
 
 struct ContentView: View {
     var body: some View {
---
+++ MangasAndComics/Pods/Target Support Files/Pods-MangasAndComicsTests/Pods-MangasAndComicsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MangasAndComicsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MangasAndComicsTests
+@end
