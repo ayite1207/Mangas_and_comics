6a8ca4348399b9bdb72138fc640cf8a18720d204
afollySqli <afolly@sqli.com>
2023-01-11 22:02:49 +0100

    add project

---
+++ MangasAndComics/MangasAndComics/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MangasAndComics
+//
+//  Created by Ayité FOLLY on 11/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MangasAndComics/MangasAndComics/MangasAndComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangasAndComicsApp.swift
+//  MangasAndComics
+//
+//  Created by Ayité FOLLY on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MangasAndComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
